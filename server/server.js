f551751a8b1bca4bc242c9070d2f6d9777548840
Subham Gope <subhamgopebusiness@gmail.com>
2025-01-04 15:11:24 +0530

    cors origin setup updated to the front end URL

--- server/server.js
+++ server/server.js
@@ -8,8 +8,15 @@ import imageRouter from "./routes/imageRoutes.js";
 const PORT = process.env.PORT;
 const app = express();
 
+const corsOptions = {
+  origin: "https://imagify-frontend-taupe.vercel.app",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true, // Allow credentials such as cookies, authorization headers, etc.
+};
+
 app.use(express.json());
-app.use(cors());
+app.use(cors(corsOptions));
 await connectDB();
 
 app.use("/api/user", userRouter);
